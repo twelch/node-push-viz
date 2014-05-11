4f9683724823094b06c686d8e75761304914f6a4
Tim Welch <tim.j.welch@gmail.com>
2014-05-11 11:53:57 -0700

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var port = 8080;
+app.use(express.static(__dirname + '/public')); 
+
+var io = require('socket.io').listen(app.listen(port));
+
+// Send data to all connected clients
+function sendVizData() {
+    io.sockets.emit('viz-load', { time: new Date().toJSON() });
+}
+// Send data every 5 seconds
+setInterval(sendVizData, 5000);
+
+//Initial handshake
+io.sockets.on('connection', function(socket) {
+    socket.emit('init-server', {message:'Server connection established'});
+    socket.on('init-client', console.log);
+});
+
+console.log("Listening on port " + port);
