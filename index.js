bcf0e8193724e2bed40c004d45050299a892f0e0
Tim Welch <tim.j.welch@gmail.com>
2014-05-11 17:16:26 -0700

    drop port from client

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 8080;
+var port = 5000;
 app.use(express.static(__dirname + '/public')); 
 
 var io = require('socket.io').listen(app.listen(port));
